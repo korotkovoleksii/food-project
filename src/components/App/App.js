039dcc9e1b6a9a1857626059b74db644946e1d0d
alekseikorotkov <korotkovaleksei@icloud.com>
2022-10-14 15:37:55 +0300

    feat: init project and header and footer

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <main className="container content"></main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
